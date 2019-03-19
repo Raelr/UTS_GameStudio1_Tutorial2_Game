ac14d156116578933a4b41701129218fdf338283
aryeh <aryehzinn@gmail.com>
2019-03-19 23:29:00 +1100

    Added player controller and sample platformer scene.

---
+++ UTS_GameStudio1_Tutorial2_Game/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    [SerializeField]
+    Player player;
+
+    [SerializeField]
+    Transform cameraFocus;
+
+    [SerializeField]
+    Vector3 cameraOffset;
+
+    Vector3 moveVelocity;
+
+    Vector2 moveVelocity2D;
+
+    float velocity;
+
+    [SerializeField]
+    float smoothCameraTiming;
+
+    Vector3 oldPosition;
+
+    [SerializeField]
+    float cameraPanSpeed;
+
+    private void Awake() {
+
+        if (cameraFocus != null) {
+            transform.position = cameraFocus.position + cameraOffset;
+        }
+
+        FollowPlayer();
+    }
+
+    void Start() {
+
+        if (player != null) {
+            player.playerMoved += FollowPlayer;
+            FollowPlayer();
+        }
+    }
+
+    public void FollowPlayer() {
+
+        if (!Utilities.VectorEquals(cameraFocus.position, oldPosition)) {
+
+            Vector3 cameraPosition = cameraFocus.position + cameraOffset;
+
+            Vector3 smoothMove = Vector3.SmoothDamp(transform.position, cameraPosition, ref moveVelocity, smoothCameraTiming * Time.deltaTime);
+
+            transform.position = smoothMove;
+
+            oldPosition = cameraFocus.position;
+        }
+    }
+
+    public void MoveCamera(Vector2 newPosition) {
+
+        Vector2 desiredLocation = newPosition * cameraPanSpeed;
+
+        Vector2 smoothPosition = Vector2.SmoothDamp(newPosition, desiredLocation, ref moveVelocity2D, smoothCameraTiming);
+
+        Vector2 coordinates = smoothPosition * Time.deltaTime;
+
+        transform.Translate(coordinates);
+    }
+}
