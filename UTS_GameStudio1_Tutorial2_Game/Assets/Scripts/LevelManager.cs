a149c3d89b9b32e75d47dde671e14662653e310e
Sam Alexandropoulos <samalex91@outlook.com>
2019-03-22 16:46:30 +1100

    Added UI Elements/Timer
    Added UI Elements to show score. coins, world and time remaining. Also added a function to countdown the timer to 0 before killing Mario.

--- UTS_GameStudio1_Tutorial2_Game/Assets/Scripts/LevelManager.cs
+++ UTS_GameStudio1_Tutorial2_Game/Assets/Scripts/LevelManager.cs
@@ -1,24 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Boo.Lang;
+using UnityEditor.Experimental.UIElements;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
     public static LevelManager instance;
+    private int _score = 0;
+    private int _coins = 0;
+    private float _time = 400f;
+    public double TimeReductionRate = 2.5;
+    public Text ScoreText;
+    public Text CoinsText;
+    public Text TimeText;
 
     [Header("Player reference")]
     [SerializeField]
     Player player;
 
     private void Awake() {
-        
-        if (instance == null) {
+
+        if (instance == null)
+        {
             instance = this;
         }
     }
 
-    public void OnPlayerKilled() {
+    void Update() {
+        ScoreText.text = "MARIO\n" + _score.ToString("000000");
+        CoinsText.text = _coins.ToString("00");
+        TimeText.text = "TIME\n" + _time.ToString("000");
+        _time -= Time.deltaTime * (float)TimeReductionRate;
+        if ((int)_time == 0){
+            TimeReductionRate = 0;
+            OnPlayerKilled();
+        }
+    }
 
+    public void OnPlayerKilled() {
         player.OnDeath();
     }
 }
