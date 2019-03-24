dc986232047ea030dc26d3907d2622402c74b651
aryeh <aryehzinn@gmail.com>
2019-03-24 22:22:44 +1100

    Added platformUser Script

---
+++ UTS_GameStudio1_Tutorial2_Game/Assets/Scripts/PlatformerScripts/PlatformUser.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PlatformUser : MonoBehaviour
+{
+    protected Collider2D currentPlatformCollider;
+
+    protected Platform currentPlatform;
+
+    protected void CheckCurrentCollider(RaycastHit2D hit) {
+
+        if (hit.transform.tag == "Platform" && hit.collider != currentPlatformCollider) {
+
+            currentPlatformCollider = hit.collider;
+            Platform platform = hit.transform.GetComponent<Platform>();
+            currentPlatform = platform == null ? null : platform;
+        }
+    }
+
+    protected abstract void CheckForTrigger(RaycastHit2D hit);
+
+    protected abstract bool IgnoreCollisions(RaycastHit2D hit, float direction = 0);
+}
