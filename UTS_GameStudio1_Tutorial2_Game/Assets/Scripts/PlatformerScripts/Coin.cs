b6c504e3f8e74fb73f758a73ec1539ca190a51c9
Sam Alexandropoulos <samalex91@outlook.com>
2019-03-25 15:19:37 +1100

    Ported over Ui_Coins branch
    After difficulty with merging into the master branch, recreated all the changes to attempt a second merge

---
+++ UTS_GameStudio1_Tutorial2_Game/Assets/Scripts/PlatformerScripts/Coin.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    void Start() {
+        
+    }
+    
+    void Update() {
+        
+    }
+
+    private void OnTrigger() {
+        Destroy(this);
+        LevelManager.instance.pickUpCoin();
+    }
+}
